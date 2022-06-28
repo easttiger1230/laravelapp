097956b38a31875303a944d45827a8627efcfee2
easttiger1230 <easttiger.1230@au.com>
2022-06-28 14:10:28 +0900

    テキストP１６４まで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,27 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
-  
-   public function index(Request $request)
-   {
-       return view('hello.index', ['msg'=>'フォームを入力：']);
-   }
 
+    public function index(Request $request)
+    {
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index', ['msg' => $msg]);
+    }
 
-   public function post(Request $request)
-   {
-       $validate_rule = [
-           'name' => 'required',
-           'mail' => 'email',
-           'age' => 'numeric|between:0,150',
-       ];
-       $this->validate($request, $validate_rule);
-       return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }
-
+    public function post(Request $request)
+    {
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view(
+            'hello.index',
+            ['msg' => '「' . $msg .
+                '」をクッキーに保存しました。']
+        );
+        $response->cookie('msg', $msg, 100);
+        return $response;
+    }
 }
