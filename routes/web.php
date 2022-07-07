9da0ea8fa068ac9077621cb9afb8b68309da9b43
easttiger1230 <easttiger.1230@au.com>
2022-07-07 11:31:41 +0900

    P259 モデルの新規保存

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,10 @@ Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 
+//---------モデルの新規保存-----------//
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+
 
 /////////////////////実習問題/////////////////////
 
